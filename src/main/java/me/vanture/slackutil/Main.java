8e61ec7217f03348ea2141225505475992f03b29
Vanturian <sadrigby@gmail.com>
2016-11-06 18:48:37 -0800

    Get rid of  log

--- src/main/java/me/vanture/slackutil/Main.java
+++ src/main/java/me/vanture/slackutil/Main.java
@@ -33,16 +33,12 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) {
-        log("Attempting to send a message to Slack!");
+        System.out.println("SlackUtil: Attempting to send a message to Slack!");
         try {
-            log("Message is transferred to Slack!");
+            System.out.println("SlackUtil: Message is transferred to Slack!");
             UtilSlack.postMessage("bots", "I'm a really cool bot :D");
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void log(String msg) {
-        System.out.println("[INFO] " + msg);
     }
 }
